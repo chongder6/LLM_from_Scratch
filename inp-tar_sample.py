9881dcf7dcaf84ddaba19aa4e19e30fee6983746
Arijit Chongder <39260192+chongder6@users.noreply.github.com>
2026-04-02 13:38:27 +0530

    Update inp-tar_sample.py

--- inp-tar_sample.py
+++ inp-tar_sample.py
@@ -52,3 +52,38 @@ y = enc_sample[1:context_size+1]
 
 print(f"x: {x}")
 print(f"y:      {y}")
+
+
+for i in range (1, context_size+1):
+  context = enc_sample[:i] #basically input
+  desired = enc_sample[i] #basically target
+
+  print(context, "--->", desired)
+
+
+#now let's see it in text format just decoding
+
+for i in range(1, context_size+1):
+
+  context = enc_sample[:i]
+  desired = enc_sample[i]
+
+  print(tokenizer.decode(context),"---->",tokenizer.decode([desired])) #here i have putted desired in a [] because it's a single word(integer) otherwise it will throw error
+
+
+# Uptil now we have we have created the input - target pairs
+# later we will be doing parallel processing
+
+# We need to implement a data loader now 
+
+# Now for implementing a efficient data loader, we will be using Pytorch's built-in **dataset and dataloader** classes and also need to follow few steps
+
+# Step1: Tokenization of entire text
+
+# Step2: Use a sliding window to chunk the book(data) into overlappung sequences of max_length
+
+# Step3: Return the total number of rows in dataset
+
+# Step4: Return a Single row from the dataset at first
+
+# *one more thing we will be calling it like input tensors and output(target) tensors*
